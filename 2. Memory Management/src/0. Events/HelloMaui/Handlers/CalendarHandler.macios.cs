a4d96aa444d3b1bfaf8f295bc907b2727eb13724
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-12-18 16:59:04 -0800

    Update Calendar on iOS + MacCatalyst

--- 2. Memory Management/src/0. Events/HelloMaui/Handlers/CalendarHandler.macios.cs
+++ 2. Memory Management/src/0. Events/HelloMaui/Handlers/CalendarHandler.macios.cs
@@ -1,117 +1,107 @@
 using Foundation;
 using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Platform;
-using ObjCRuntime;
 using UIKit;
 
 namespace HelloMaui.Handlers;
 
 public partial class CalendarHandler : ViewHandler<ICalendarView, UICalendarView>, IDisposable
 {
-	UICalendarSelection? _calendarSelection;
-	
-	~CalendarHandler()
-	{
-		Dispose(false);
-	}
+    UICalendarSelection? _calendarSelection;
 
-	public void Dispose()
-	{
-		Dispose(true);
-		GC.SuppressFinalize(this);
-	}
-	
-	protected override UICalendarView CreatePlatformView()
-	{
-		return new UICalendarView();
-	}
+    ~CalendarHandler()
+    {
+        Dispose(false);
+    }
 
-	protected override void ConnectHandler(UICalendarView platformView)
-	{
-		base.ConnectHandler(platformView);
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
 
-		_calendarSelection = new UICalendarSelectionSingleDate(new CalendarSelectionSingleDateDelegate(VirtualView));
-	}
-	
-	protected virtual void Dispose(bool disposing)
-	{
-		ReleaseUnmanagedResources();
-		
-		if (disposing)
-		{
-			_calendarSelection?.Dispose();
-			_calendarSelection = null;
-		}
-	}
+    protected override UICalendarView CreatePlatformView()
+    {
+        _calendarSelection = new UICalendarSelectionSingleDate(new CalendarSelectionSingleDateDelegate(VirtualView));
+        return new UICalendarView
+        {
+            Calendar = new(NSCalendarType.Gregorian),
+            SelectionBehavior = _calendarSelection
+        };
+    }
 
-	static void MapSelectedDate(CalendarHandler handler, ICalendarView virtualView)
-	{
-		if (handler._calendarSelection is UICalendarSelectionSingleDate calendarSelection)
-		{
-			MapSingleDateSelection(calendarSelection, virtualView);
-		}
-	}
-	
-	static void MapSingleDateSelection(UICalendarSelectionSingleDate calendarSelection, ICalendarView virtualView)
-	{
-		if (virtualView.SelectedDate is null)
-		{
-			calendarSelection.SetSelectedDate(null, true);
-			return;
-		}
+    protected virtual void Dispose(bool disposing)
+    {
+        ReleaseUnmanagedResources();
 
-		calendarSelection.SetSelectedDate(new NSDateComponents
-		{
-			Day = virtualView.SelectedDate.Value.Day,
-			Month = virtualView.SelectedDate.Value.Month,
-			Year = virtualView.SelectedDate.Value.Year
-		}, true);
-	}
+        if (disposing)
+        {
+            _calendarSelection?.Dispose();
+            _calendarSelection = null;
+        }
+    }
+
+    static void MapSelectedDate(CalendarHandler handler, ICalendarView virtualView)
+    {
+        if (handler._calendarSelection is UICalendarSelectionSingleDate calendarSelection)
+        {
+            MapSingleDateSelection(calendarSelection, virtualView);
+        }
+    }
+
+    static void MapSingleDateSelection(UICalendarSelectionSingleDate calendarSelection, ICalendarView virtualView)
+    {
+        if (virtualView.SelectedDate is null)
+        {
+            calendarSelection.SetSelectedDate(null, true);
+            return;
+        }
+
+        calendarSelection.SetSelectedDate(new NSDateComponents
+        {
+            Day = virtualView.SelectedDate.Value.Day,
+            Month = virtualView.SelectedDate.Value.Month,
+            Year = virtualView.SelectedDate.Value.Year
+        }, true);
+    }
 
 
-	static void MapFirstDayOfWeek(CalendarHandler handler, ICalendarView virtualView)
-	{
-		handler.PlatformView.Calendar.FirstWeekDay = (nuint)virtualView.FirstDayOfWeek;
-	}
-	
-	static void MapMinDate(CalendarHandler handler, ICalendarView virtualView)
-	{
-		SetDateRange(handler, virtualView);
-	}
-	
-	static void MapMaxDate(CalendarHandler handler, ICalendarView virtualView)
-	{
-		SetDateRange(handler, virtualView);
-	}
-	
-	static void SetDateRange(CalendarHandler handler, ICalendarView virtualView)
-	{
-		var fromDateComponents = virtualView.MinDate.Date.ToNSDate();
-		var toDateComponents = virtualView.MaxDate.Date.ToNSDate();
+    static void MapFirstDayOfWeek(CalendarHandler handler, ICalendarView virtualView)
+    {
+        handler.PlatformView.Calendar.FirstWeekDay = (nuint)virtualView.FirstDayOfWeek;
+    }
 
-		var calendarViewDateRange = new NSDateInterval(fromDateComponents, toDateComponents);
-		handler.PlatformView.AvailableDateRange = calendarViewDateRange;
-	}
+    static void MapMinDate(CalendarHandler handler, ICalendarView virtualView)
+    {
+        SetDateRange(handler, virtualView);
+    }
 
-	void ReleaseUnmanagedResources()
-	{
-		// TODO release unmanaged resources here
-	}
-	
-	sealed class CalendarSelectionSingleDateDelegate(ICalendarView calendarView) : IUICalendarSelectionSingleDateDelegate
-	{
-		public NativeHandle Handle { get; }
-		
-		public void Dispose()
-		{
-			
-		}
+    static void MapMaxDate(CalendarHandler handler, ICalendarView virtualView)
+    {
+        SetDateRange(handler, virtualView);
+    }
 
-		public void DidSelectDate(UICalendarSelectionSingleDate calendarSelection, NSDateComponents? date)
-		{
-			calendarSelection.SelectedDate = date;
-			calendarView.SelectedDate = date?.Date.ToDateTime();
-			calendarView.OnSelectedDateChanged(date?.Date.ToDateTime());
-		}
-	}
+    static void SetDateRange(CalendarHandler handler, ICalendarView virtualView)
+    {
+        var fromDateComponents = virtualView.MinDate.Date.ToNSDate();
+        var toDateComponents = virtualView.MaxDate.Date.ToNSDate();
+
+        var calendarViewDateRange = new NSDateInterval(fromDateComponents, toDateComponents);
+        handler.PlatformView.AvailableDateRange = calendarViewDateRange;
+    }
+
+    void ReleaseUnmanagedResources()
+    {
+        // TODO release unmanaged resources here
+    }
+
+    sealed class CalendarSelectionSingleDateDelegate(ICalendarView calendarView) : UICalendarSelectionSingleDateDelegate
+    {
+        public override void DidSelectDate(UICalendarSelectionSingleDate selection, NSDateComponents? dateComponents)
+        {
+            selection.SelectedDate = dateComponents;
+            calendarView.SelectedDate = dateComponents?.Date.ToDateTime();
+            calendarView.OnSelectedDateChanged(dateComponents?.Date.ToDateTime());
+        }
+    }
 }
